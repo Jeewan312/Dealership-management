8c534df1b356c0a3d31c3fd145f2a9883bf52c2d
Jeewan312 <jeewanbashyal312@gmail.com>
2025-11-04 21:11:25 +0545

    code added

---
+++ design/javscript/signin.js
@@ -0,0 +1,13 @@
+document.getElementById("registerForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match!");
+  } else {
+    alert("Account created successfully!");
+    this.reset();
+  }
+});
---
+++ visitor/homepage.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../design/css/homepage.css" />
+    <style> </style>
+    <title>Navowheels automobile</title>
+</head>
+
+<body>
+    <!-- this is for nav bar bar  -->
+    <div class="nav">
+        <div class="logo">
+            <img src="../uploads/images/Navowheels.png" alt="" class="logo" />
+        </div>
+        <div class="nav-link">
+            <ul>
+                <li> <a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li> <a href="#">Service</a></li>
+                <li> <a href="#">Product</a></li>
+                <li><a href="login.php">login</a></li>
+            </ul>
+        </div>
+    </div>
+    <div id="hero">
+        <img src="../uploads/images/bike.background.jpg" alt="Bike" class="background" />
+        <div class="hero-text">
+            <h1>
+                Welcome to <span class="accent">Navowheels</span><br>
+                Ride the <span class="accent2">Future</span>
+            </h1>
+            <p>Where <span class="accent">power</span> meets <span class="accent2">innovation</span> on two wheels.</p>
+            <button>Discover More</button>
+        </div>
+    </div>
+    <div>
+    </div>
+</body>
+
+</html>
